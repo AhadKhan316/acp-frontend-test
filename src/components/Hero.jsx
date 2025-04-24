5dd45e12b9cbcf54176bf88d1e9dc0bfe3b96342
AhadKhan316 <140253722+AhadKhan316@users.noreply.github.com>
2025-04-24 15:41:07 +0500

    src/Pages/GoverningBody.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,9 @@ import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { ArrowRight } from 'lucide-react';
 
-import HeroImg1 from "/src/assets/acp-logo-and-hero-img/acp-hero3.jpg";
-import HeroImg2 from "/src/assets/acp-logo-and-hero-img/acp-hero1.jpg";
-import HeroImg3 from "/src/assets/acp-logo-and-hero-img/acp-hero2.jpg";
+import HeroImg1 from "/src/assets/acp-logo-and-hero-img/acp-hero1.jpg";
+import HeroImg2 from "/src/assets/acp-logo-and-hero-img/acp-hero2.jpg";
+import HeroImg3 from "/src/assets/acp-logo-and-hero-img/acp-hero3.jpg";
 
 const Hero = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
